11797f5597bcfc4c1f0bb52ec138081f1a9cca0f
EdytaL <lysiakedyta@gmail.com>
2018-04-08 22:06:07 +0200

    WIP: create api add trip, TODO: fix model TripDetails for connecions

--- backend/models/tripDetails.js
+++ backend/models/tripDetails.js
@@ -6,107 +6,108 @@ var tripDetailsSchema = {
 
     "bookingCode": String,
     "itinerary": {
-    "type": String,
-        "connections": [
-        {
-            "duration": String,
-            "origin": {
-                "IATACode": String,
-                "name": String,
-                "city": {
-                    "IATACode": String,
-                    "name": String,
-                    "country": {
-                        "code": String,
-                        "name": String
-                    }
-                }
-            },
-            "destination": {
-                "IATACode": String,
-                "name": String,
-                "city": {
-                    "IATACode": String,
-                    "name": String,
-                    "country": {
-                        "code": String,
-                        "name": String
-                    }
-                },
-            },
-            "segments": [
-                {
-                    "type": String,
-                    "informational": Boolean,
-                    "departFrom": {
-                        "IATACode": String,
-                        "name": String,
-                        "city": {
-                            "IATACode": String,
-                            "name": String,
-                            "country": {
-                                "code": String,
-                                "name": String
-                            }
-                        },
-                    },
-                    "arriveOn": {
-                        "IATACode": String,
-                        "name": String,
-                        "city": {
-                            "IATACode": String,
-                            "name": String,
-                            "country": {
-                                "code": String,
-                                "name": String
-                            }
-                        },
-                    },
-                    "marketingFlight": {
-                        "number": String,
-                        "carrier": {
-                            "code": String,
-                            "name": String
-                        },
-                        "status": {
-                            "code": String,
-                            "name": String
-                        },
-                        "numberOfStops": 0,
-                        "sellingClass": {
-                            "code": String
-                        },
-                        "operatingFlight": {
-                            "number": String,
-                            "carrier": {
-                                "code": String,
-                                "name": String
-                            },
-                            "duration": String,
-                            "flown": Boolean,
-                            "checkInStart": String,
-                            "localCheckInStart": String,
-                            "checkInEnd": String,
-                            "localCheckInEnd": String,
-                            "scheduledArrival": String,
-                            "localScheduledArrival": String,
-                            "scheduledDeparture": String,
-                            "localScheduledDeparture": String,
-                            "arrivalTerminal": {
-                                "name": String
-                            },
-                            "cabin": {
-                                "code": String,
-                                "name": String
-                            },
-                            "equipment": {
-                                "code": String,
-                                "name": String
-                            }
-                        }
-                    }
-                }]
-        }]
+        "type": String,
+        "connections": Object
+            // [
+        // {
+        //     "duration": String,
+        //     "origin": {
+        //         "IATACode": String,
+        //         "name": String,
+        //         "city": {
+        //             "IATACode": String,
+        //             "name": String,
+        //             "country": {
+        //                 "code": String,
+        //                 "name": String
+        //             }
+        //         }
+        //     },
+        //     "destination": {
+        //         "IATACode": String,
+        //         "name": String,
+        //         "city": {
+        //             "IATACode": String,
+        //             "name": String,
+        //             "country": {
+        //                 "code": String,
+        //                 "name": String
+        //             }
+        //         },
+        //     },
+        //     "segments": [
+        //         {
+        //             "type": String,
+        //             "informational": Boolean,
+        //             "departFrom": {
+        //                 "IATACode": String,
+        //                 "name": String,
+        //                 "city": {
+        //                     "IATACode": String,
+        //                     "name": String,
+        //                     "country": {
+        //                         "code": String,
+        //                         "name": String
+        //                     }
+        //                 },
+        //             },
+        //             "arriveOn": {
+        //                 "IATACode": String,
+        //                 "name": String,
+        //                 "city": {
+        //                     "IATACode": String,
+        //                     "name": String,
+        //                     "country": {
+        //                         "code": String,
+        //                         "name": String
+        //                     }
+        //                 },
+        //             },
+        //             "marketingFlight": {
+        //                 "number": String,
+        //                 "carrier": {
+        //                     "code": String,
+        //                     "name": String
+        //                 },
+        //                 "status": {
+        //                     "code": String,
+        //                     "name": String
+        //                 },
+        //                 "numberOfStops": 0,
+        //                 "sellingClass": {
+        //                     "code": String
+        //                 },
+        //                 "operatingFlight": {
+        //                     "number": String,
+        //                     "carrier": {
+        //                         "code": String,
+        //                         "name": String
+        //                     },
+        //                     "duration": String,
+        //                     "flown": Boolean,
+        //                     "checkInStart": String,
+        //                     "localCheckInStart": String,
+        //                     "checkInEnd": String,
+        //                     "localCheckInEnd": String,
+        //                     "scheduledArrival": String,
+        //                     "localScheduledArrival": String,
+        //                     "scheduledDeparture": String,
+        //                     "localScheduledDeparture": String,
+        //                     "arrivalTerminal": {
+        //                         "name": String
+        //                     },
+        //                     "cabin": {
+        //                         "code": String,
+        //                         "name": String
+        //                     },
+        //                     "equipment": {
+        //                         "code": String,
+        //                         "name": String
+        //                     }
+        //                 }
+        //             }
+        //         }]
+        // }]
     },
     "passenger": {
         "firstName": String,
@@ -119,6 +120,6 @@ var tripDetailsSchema = {
 
 };
 
-var TripDetails = mongoose.model('TripDetails', tripDetailsSchema);
+var TripDetails = mongoose.model('TripDetails', tripDetailsSchema, 'tripdetails');
 
 module.exports = TripDetails;
